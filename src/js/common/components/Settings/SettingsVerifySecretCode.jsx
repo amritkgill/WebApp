d365dc96caecd09c6c0ef4c41e0b14617c172227
stevepodell <steve@podell.com>
2025-06-26 09:40:24 -0700

    WV-1188: Handle very long email addresses in the SettingsVerifySecretCode dialog. Also move the import of `https://fonts.googleapis.com/css2?family=Poppins...` from main.css to index.html for Cordova

--- src/js/common/components/Settings/SettingsVerifySecretCode.jsx
+++ src/js/common/components/Settings/SettingsVerifySecretCode.jsx
@@ -507,6 +507,13 @@ class SettingsVerifySecretCode extends Component {
       return null;
     }
 
+    let sentTo = voterPhoneNumber || voterEmailAddress;
+    // Parambatur.Narayanasami@wevoteeducation.org  6/25/25: Long names were making the dialog horizont123ally scrollable
+    if (voterEmailAddress.length > 25) {
+      const pieces = voterEmailAddress.split('@');
+      sentTo = `${pieces[0]}@\n${pieces[1]}`;
+    }
+
     return (
       <Dialog
         id="codeVerificationDialog"
@@ -536,7 +543,7 @@ class SettingsVerifySecretCode extends Component {
           <TextContainer>
             <Title condensed={condensed}>Code Verification</Title>
             <Subtitle>A 6-digit code has been sent to</Subtitle>
-            <PhoneSubtitle>{voterPhoneNumber || voterEmailAddress}</PhoneSubtitle>
+            <PhoneSubtitle>{sentTo}</PhoneSubtitle>
 
             {(voterEmailAddress) ? (
               <Subtitle>If you haven&apos;t received the code in 30 seconds, please check your spam folder and mark the email as &apos;Not Spam&apos;.</Subtitle>
